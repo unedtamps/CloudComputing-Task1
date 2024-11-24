e22bb89f56febed0b343df614cfbb0b12b23d27d
unetamps <unedo.tampubolon@gmail.com>
2024-11-25 02:16:24 +0700

    edit network config

--- opentofu/variables.tf
+++ opentofu/variables.tf
@@ -1,5 +1,5 @@
 variable "db_vm_name" {
-  default = "db_vm10"
+  default = "db_vm0"
 }
 
 variable "db_vm_memory" {
@@ -11,7 +11,7 @@ variable "db_vm_vcpus" {
 }
 
 variable "web_vm_name" {
-  default = "web_vm1"
+  default = "web_vm0"
 }
 
 variable "web_vm_memory" {
